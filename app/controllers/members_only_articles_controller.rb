4dc03773fa67848a93356e6a8707ca06070194d8
fartun <fartunwario34@gmail.com>
2023-07-02 18:20:20 +0300

    completed assigment

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -2,19 +2,22 @@ class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
-    articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
-    render json: articles, each_serializer: ArticleListSerializer
+    articles = Article.where(is_member_only: true)
+    render json: articles
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    if current_user
+      article = Article.find(params[:id])
+      render json: article
+    else
+      render json: { error: 'Unauthorized - Sign in required' }, status: :unauthorized
+    end
   end
 
   private
 
-  def record_not_found
-    render json: { error: "Article not found" }, status: :not_found
+  def current_user
+    current_user # Assuming you have Devise configured and the user model is called User
   end
-
 end
